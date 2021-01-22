9c900e0adee8d9c5a1b48a09c37e8237240fe7e0
Andres Almiray <aalmiray@gmail.com>
2021-01-22 13:50:21 +0100

    Initial gradle setup

---
+++ refactor-first-gradle-plugin/src/main/java/org/hjug/gradlereport/RefactorFirstPlugin.java
@@ -0,0 +1,10 @@
+package org.hjug.gradlereport;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class RefactorFirstPlugin implements Plugin<Project> {
+    public void apply(Project project) {
+        // impl
+    }
+}
