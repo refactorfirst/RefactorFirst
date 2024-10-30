e469851cab7e0ec074f98d950730619965b2ada5
Jim Bethancourt <jimbethancourt@gmail.com>
2024-10-29 20:18:47 -0500

    #111 Extract FqnCapturingVisitor to simplify multi-language visiting
    Extract FqnCapturingVisitor to simplify implementation of multi-language visiting

--- circular-reference-detector/src/main/java/org/hjug/parser/JavaFqnCapturingVisitor.java
+++ circular-reference-detector/src/main/java/org/hjug/parser/JavaFqnCapturingVisitor.java
@@ -1,8 +1,6 @@
 package org.hjug.parser;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import lombok.Getter;
 import org.openrewrite.java.JavaIsoVisitor;
@@ -10,63 +8,19 @@ import org.openrewrite.java.tree.J;
 
 /**
  * Captures Fully Qualified Names (FQN) of classes as they will be imported in import statements.
- * Output used to resolve Generic types.
+ * fqns map that is populated by this visitor is used to resolve Generic types.
  *
  * @param <P>
  */
 @Getter
-public class JavaFqnCapturingVisitor<P> extends JavaIsoVisitor<P> {
+public class JavaFqnCapturingVisitor<P> extends JavaIsoVisitor<P> implements FqnCapturingVisitor {
 
+    // consider using ConcurrentHashMap to scale performance
     // package -> name, FQN
     private final Map<String, Map<String, String>> fqns = new HashMap<>();
 
     @Override
     public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, P p) {
-        // get class fqn (including "$")
-        String fqn = classDecl.getType().getFullyQualifiedName();
-
-        String currentPackage = getPackage(fqn);
-        String className = getClassName(fqn);
-        Map<String, String> classesInPackage = fqns.getOrDefault(currentPackage, new HashMap<>());
-
-        if (className.contains("$")) {
-            String normalizedClassName = className.replace('$', '.');
-            List<String> parts = Arrays.asList(normalizedClassName.split("\\."));
-            for (int i = 0; i < parts.size(); i++) {
-                String key = String.join(".", parts.subList(i, parts.size()));
-                classesInPackage.put(key, currentPackage + "." + normalizedClassName);
-            }
-        } else {
-            classesInPackage.put(className, fqn);
-        }
-
-        fqns.put(currentPackage, classesInPackage);
-        return super.visitClassDeclaration(classDecl, p);
-    }
-
-
-    String getPackage(String fqn) {
-        // handle no package
-        if (!fqn.contains(".")) {
-            return "";
-        }
-
-        int lastIndex = fqn.lastIndexOf(".");
-        return fqn.substring(0, lastIndex);
-    }
-
-    /**
-     *
-     * @param fqn
-     * @return Class name (including "$") after last period in FQN
-     */
-    String getClassName(String fqn) {
-        // handle no package
-        if (!fqn.contains(".")) {
-            return fqn;
-        }
-
-        int lastIndex = fqn.lastIndexOf(".");
-        return fqn.substring(lastIndex + 1);
+        return super.visitClassDeclaration(captureClassDeclarations(classDecl, fqns), p);
     }
 }
