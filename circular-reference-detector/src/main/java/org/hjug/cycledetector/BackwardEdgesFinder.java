e04354ad1bbef7144f7d13b1ae86b2b967194a48
Jim Bethancourt <jimbethancourt@gmail.com>
2025-02-11 19:37:05 -0600

    #130 Tracking the number of cycles a backward edge participates in
    - Tracking the number of cycles a backward edge participates in
    - Added unit test
    - Added comments

--- circular-reference-detector/src/main/java/org/hjug/cycledetector/BackwardEdgesFinder.java
+++ circular-reference-detector/src/main/java/org/hjug/cycledetector/BackwardEdgesFinder.java
@@ -10,6 +10,8 @@ Prompt:
 provide a java implementation of an algorithm that will identify all backward edges in a cycle
 identified by JGraphT that is part of a graph with weighted edges that have integer weights.
 If there are multiple backward edges, order the backward edges in the order they should be removed.
+
+Modified slightly to track the number of cycles a backward edge participates in
  */
 public class BackwardEdgesFinder<V> {
 
@@ -19,32 +21,37 @@ public class BackwardEdgesFinder<V> {
         this.graph = graph;
     }
 
-    public List<DefaultWeightedEdge> findBackwardEdges() {
+    public Map<DefaultWeightedEdge, Integer> findBackwardEdges() {
         TarjanSimpleCycles<V, DefaultWeightedEdge> tarjan = new TarjanSimpleCycles<>(graph);
         List<List<V>> cycles = tarjan.findSimpleCycles();
-        List<DefaultWeightedEdge> backwardEdges = new ArrayList<>();
+        Map<DefaultWeightedEdge, Integer> backwardEdgesWithCounts = new TreeMap<>(((Comparator<DefaultWeightedEdge>)
+                        // compare based on weight
+                        (e1, e2) -> Integer.compare((int) graph.getEdgeWeight(e1), (int) graph.getEdgeWeight(e2)))
+                // then compare based on name (this will result in edges being in alpha order - not exactly ideal)
+                .thenComparing(DefaultWeightedEdge::toString));
 
         for (List<V> cycle : cycles) {
             Set<DefaultWeightedEdge> cycleEdges = new HashSet<>();
             for (int i = 0; i < cycle.size(); i++) {
                 V source = cycle.get(i);
+                // getting (i + 1) % cycle size produces the next node added to the cycle
                 V target = cycle.get((i + 1) % cycle.size());
                 DefaultWeightedEdge edge = graph.getEdge(source, target);
+                // if there is an edge between the nodes, add it to cycleEdges
                 if (edge != null) {
                     cycleEdges.add(edge);
                 }
             }
 
             for (DefaultWeightedEdge edge : cycleEdges) {
-                if (isBackwardEdge(edge, cycle) && !backwardEdges.contains(edge)) {
-                    backwardEdges.add(edge);
+                if (isBackwardEdge(edge, cycle)) {
+                    backwardEdgesWithCounts.put(edge, backwardEdgesWithCounts.getOrDefault(edge, 0) + 1);
                 }
             }
         }
 
         // The ordering is naive, but it's a start
-        backwardEdges.sort(Comparator.comparingDouble(graph::getEdgeWeight));
-        return backwardEdges;
+        return backwardEdgesWithCounts;
     }
 
     private boolean isBackwardEdge(DefaultWeightedEdge edge, List<V> cycle) {
--- circular-reference-detector/src/test/java/org/hjug/cycledetector/BackwardEdgesFinderTest.java
+++ circular-reference-detector/src/test/java/org/hjug/cycledetector/BackwardEdgesFinderTest.java
@@ -1,10 +1,13 @@
 package org.hjug.cycledetector;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BackwardEdgesFinderTest {
@@ -25,12 +28,43 @@ class BackwardEdgesFinderTest {
         graph.setEdgeWeight(graph.addEdge("C", "A"), 5);
 
         BackwardEdgesFinder<String> finder = new BackwardEdgesFinder<>(graph);
-        List<DefaultWeightedEdge> backwardEdges = finder.findBackwardEdges();
+        Map<DefaultWeightedEdge, Integer> backwardEdgesWithCounts = finder.findBackwardEdges();
+        List<DefaultWeightedEdge> backwardEdges = new ArrayList<>(backwardEdgesWithCounts.keySet());
 
-        Assertions.assertEquals("(D : A)", backwardEdges.get(0).toString());
-        Assertions.assertEquals(4, graph.getEdgeWeight(backwardEdges.get(0)));
+        assertEquals("(D : A)", backwardEdges.get(0).toString());
+        assertEquals(4, graph.getEdgeWeight(backwardEdges.get(0)));
+        assertEquals(1, backwardEdgesWithCounts.get(backwardEdges.get(0)));
 
-        Assertions.assertEquals("(C : A)", backwardEdges.get(1).toString());
-        Assertions.assertEquals(5, graph.getEdgeWeight(backwardEdges.get(1)));
+        assertEquals("(C : A)", backwardEdges.get(1).toString());
+        assertEquals(5, graph.getEdgeWeight(backwardEdges.get(1)));
+        assertEquals(1, backwardEdgesWithCounts.get(backwardEdges.get(1)));
+    }
+
+    @Test
+    void findBackwardEdgesWithSameWeight() {
+        Graph<String, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+
+        graph.setEdgeWeight(graph.addEdge("A", "B"), 1);
+        graph.setEdgeWeight(graph.addEdge("B", "C"), 2);
+        graph.setEdgeWeight(graph.addEdge("C", "D"), 3);
+        graph.setEdgeWeight(graph.addEdge("D", "A"), 4);
+        graph.setEdgeWeight(graph.addEdge("C", "A"), 4);
+
+        BackwardEdgesFinder<String> finder = new BackwardEdgesFinder<>(graph);
+        Map<DefaultWeightedEdge, Integer> backwardEdgesWithCounts = finder.findBackwardEdges();
+        List<DefaultWeightedEdge> backwardEdges = new ArrayList<>(backwardEdgesWithCounts.keySet());
+
+        assertEquals("(C : A)", backwardEdges.get(0).toString());
+        assertEquals(4, graph.getEdgeWeight(backwardEdges.get(0)));
+        assertEquals(1, backwardEdgesWithCounts.get(backwardEdges.get(0)));
+
+        assertEquals("(D : A)", backwardEdges.get(1).toString());
+        assertEquals(4, graph.getEdgeWeight(backwardEdges.get(1)));
+        assertEquals(1, backwardEdgesWithCounts.get(backwardEdges.get(1)));
     }
 }
